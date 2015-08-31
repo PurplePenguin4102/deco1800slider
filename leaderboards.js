5fdea92aa80feda04af35b710424baaf12f04d4d
ppenguin <joseph.h.ray@gmail.com>
2015-08-31 17:15:54 +1000

    created files to be filled out

---
+++ leaderboards.js
@@ -0,0 +1,14 @@
+function getScores() {
+      //This function needs to be able to fetch a list of scores from
+      //leaderboard
+}
+
+function queryBoard(var score) {
+      //compares a number called "score" to the current leaderboard and
+      //returns a position. if the position > 50 then returns null
+}
+
+function setScore(var name, var score, var pos) {
+      //puts the name and associated score into the leaderboard (scores.txt)
+      //at position pos. Shifts every entry after that down by 1.
+}
