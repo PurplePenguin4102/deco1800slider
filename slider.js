b12eec8e11957eb1af17cd355d8b6ac9ec7ebcad
Joseph Ray <joseph.h.ray@gmail.com>
2015-09-02 10:30:28 +1000

    modified Chris' skeleton

---
+++ slider.js
@@ -0,0 +1,16 @@
+function initSlider(var image) {
+  //turn the DOM element image into a sliding puzzle
+}
+
+function mixSlider(var image) {
+  //mix up the sliding puzzle on image
+}
+
+function getScore() {
+  //returns the user's current score
+}
+
+function querySolution() {
+  //probably don't worry about this one till last, needs to autosolve
+  //the puzzle and return how many moves it took
+}
