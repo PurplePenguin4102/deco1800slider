265bb90488e568b40e70e16b48b2f66b9caad6a8
chhaynemo007 <Mr.No@d-c101-242-88.visitor.uq.net.au>
2015-09-14 17:21:29 +1000

    Revert "Finished & Tested by Mr.No"
    This reverts commit e5d813e61968043b2a0e3cb320c21b8b65fb2501.
    
    Conflicts:
    leaderboards.js

--- leaderboards.js
+++ leaderboards.js
@@ -1,20 +1,55 @@
 var empList = [];
 
-/*function queryBoard(var score) {
-      //compares a number called "score" to the current leaderboard and
-      //returns a position. if the position > 50 then returns null
-};*/
+function updateScore (a, b) {
+      //If the score input has the same name as the one in the empList, return true
+      //Update the score if the new score input is higher else do nothing
+      for (i=0; i<empList.length ; i++) {
+          if (empList[i].nick === a) {
+              if (b > empList[i].score) {
+                  if (i===0) {
+                      empList[i].score = b;
+                  }
+                  else {
+                      empList[i].score = b;
+                      var rise = 0;
+                      for (x=i; x>0; x--) {
+                          if (b > empList[x-1].score) {
+                              rise++;
+                          }
+                      }
+                      if (rise === i) {
+                          empList.unshift({nick:a, score:b});
+                      }
+                      else if (rise === 0) {
+                          null;
+                      }
+                      else {
+                          empList.splice(i,1);
+                          empList.splice(i-rise,0,{nick:a, score:b});
+                      }
+                      return true;
+                  }
+              }
+              else {
+                  return true;
+              }
+          }
+      }
+}
 
 function checkScore(a, b) {
+
+      //Create a score object and append to the empList for given name and score
+      //Sort in accending order according to their scores
       if (empList.length === 0) {
-          empList.push({name:a, score:b});
+          empList.push({nick:a, score:b});
       }
       else if (empList.length === 1) {
           if (b > empList[0].score) {
-              empList.splice(0,0,{name:a, score:b});
+              empList.splice(0,0,{nick:a, score:b});
           }
           else {
-              empList.push({name:a, score:b});
+              empList.push({nick:a, score:b});
           }
       }
       else {
@@ -22,7 +57,7 @@ function checkScore(a, b) {
           for (i=0; i <= empList.length; i++) {
               if (b === empList[i].score) {
                   break;
-                  empList.splice(i+1,0,{name:a, score:b});
+                  empList.splice(i+1,0,{nick:a, score:b});
               }
               else {
                   diff.push(b - empList[i].score);
@@ -34,10 +69,10 @@ function checkScore(a, b) {
           };
           var index = diff.indexOf(min);
           if (min > 0) {
-              empList.splice(index-1, 0, {name:a, score:b})
+              empList.splice(index-1, 0, {nick:a, score:b})
           }
           else {
-              empList.splice(index+count+1, 0, {name:a, score:b})
+              empList.splice(index+count+1, 0, {nick:a, score:b})
           }
           };
       };
@@ -55,17 +90,19 @@ function setScore(a, b) {
       };
 }
 
-setScore("Chai", 5);
-setScore("ggwp", 5);
-console.log(empList);
-setScore("fuck", 1);
-console.log(empList);
-setScore("fkfk", 5);
-console.log(empList);
-
 /*function getScores() {
       //This function needs to be able to fetch a list of scores from
-      //leaderboard and returns two lists (list of lists) containing names
+      //leaderboard and returns a list with objects containing names
       //and scores
-      return emplist;
-};*/
+      return empList;
+}*/
+
+setScore("Chai", 5);
+setScore("CB", 10);
+console.log(empList);
+setScore("gg", 2);
+console.log(empList);
+setScore("ggwp", 100);
+console.log(empList);
+setScore("fg", 7);
+console.log(empList);
